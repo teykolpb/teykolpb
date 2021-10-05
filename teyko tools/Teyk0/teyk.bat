062810d92983a942e0ab29b1847c45b89cb45ec8
teykolpb <91982766+teykolpb@users.noreply.github.com>
2021-10-05 20:46:18 +0200

    Delete teyk.bat

--- teyko tools/Teyk0/teyk.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:a
-echo press 1 for (http://www.patorjk.com/software/taag/#p=testall&f=Graffiti&t=teyk)
-echo press 2 for (http://www.patorjk.com/software/taag/#p=testall&f=Graffiti&t=teyk)
-echo press 3 for (http://www.patorjk.com/software/taag/#p=testall&f=Graffiti&t=teyk)
-set /p ans="Enter your number:"
